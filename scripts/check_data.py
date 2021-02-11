92e0ed332e43294e7a44328984469c749b4a6242
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-02-11 13:50:13 +0000

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,20 @@
 from sdg.open_sdg import open_sdg_check
 
+
+def alter_meta(meta):
+# Automatically detect global indicators.
+    if 'indicator_number' in meta:
+        indicator_id = meta['indicator_number']
+        id_parts = indicator_id.split('.')
+
+        # Automatically set some predicable properties.
+        meta['goal_number'] = id_parts[0]
+        meta['target_number'] = id_parts[0] + '.' + id_parts[1]
+        meta['target_name'] = 'global_targets.' + id_parts[0] + '-' + id_parts[1] + '-title'
+
+
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta)
 
 # If everything was valid, perform the build.
 if not validation_successful:
